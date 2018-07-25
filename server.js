5430e6c48c78143ff53c59ec23b2807a74465605
kgonzalez452 <kgonzalez@techlaunch.io>
2018-07-25 18:34:14 -0400

    first server done

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var port = 3030;
+var host = "127.0.0.1";
+
+var jsonData = { count: 12, message: 'hey' };
+
+app.get('/', (req, res) => {
+  // res.sendFile takes an ABSOLUTE path to a file and sets the mime type based on the file extension
+  res.sendFile(__dirname + '/index.html', err => {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
+app.get('/data', (req, res) => {
+  res.json(jsonData);
+})
+
+app.listen(port, host, function () {
+  console.log('Listening on http://localhost:', port);
+})
